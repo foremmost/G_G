3fbb1f026a5d08e4c6b541c0d50b46cb45028fd6
foremmost <rapid1992ghbdtn21>
2021-10-20 05:32:30 +0600

    - finded wrong logic with setter need to fix it

--- G_G.js
+++ G_G.js
@@ -1,42 +1,49 @@
 export default class G_G{
 	constructor(props){
 		const _ = this;
-	
 		_.initedUpdate = false;
 		_.handlersName = Symbol('handlers');
 		_[_.handlersName] = [];
 		_.stateName = Symbol('state');
-		_[_.stateName] = new Proxy({},{
-			get: (t,p) =>t[p],
+		let validator = {
+			get: (target,key) =>{
+				if (typeof target[key] === 'object') {
+					return new Proxy(target[key], validator)
+				} else {
+					return target[key];
+				}
+			},
 			set:(t,p,v)=>{
 				Reflect.set(t,p,v);
 				_.update([p]);
 				return true;
 			}
-		});
-		_.defineDefineMethod(props);
-		_.defineInitMethod(props);
-		_.update();
-		_.initedUpdate = true;
+		}
+		_[_.stateName] = new Proxy({},validator);
+		_.ii(props);
 	}
 	defineDefineMethod(props){
 		const _ = this;
-		if(!( (props?.define ?? 'define') in _) ){
-			throw Error('G_G: No define method declared');
-		}else{
-			_[props?.define ?? 'define']();
-		}
+		return new Promise( (resolve) =>{
+			if(!( (props?.define ?? 'define') in _) ){
+				throw Error('G_G: No define method declared');
+			}else{
+				resolve(_[props?.define ?? 'define']());
+			}
+		})
 	}
 	defineInitMethod(props){
 		const _ = this;
-		if(!( (props?.init ?? 'init') in _) ){
-			throw Error('G_G: No initialization method declared');
-		}else{
-			_[props?.init ?? 'init']();
-		}
+		return new Promise( (resolve) =>{
+			if(!( (props?.init ?? 'init') in _) ){
+				throw Error('G_G: No initialization method declared');
+			}else{
+				resolve(_[props?.init ?? 'init']());
+			}
+		})
 	}
-	
-	
+
+
 	set(state){
 		const _ = this;
 		for(let prop in state){
@@ -46,13 +53,13 @@ export default class G_G{
 		_.storage = _[_.stateName]
 		return _.storage;
 	}
-	
+
 	/* Working with Dom methods */
 	markup(domStr,isFragment=true){
 		const _ = this;
 		let
-		fragment = document.createDocumentFragment(),
-		parser= new DOMParser().parseFromString(domStr,'text/html');
+			fragment = document.createDocumentFragment(),
+			parser= new DOMParser().parseFromString(domStr,'text/html');
 		if(isFragment){
 			fragment.append(...parser.body.children);
 			return fragment;
@@ -74,8 +81,9 @@ export default class G_G{
 			domElement.innerHTML = null;
 		}
 	}
+	//171.315
 	/* Working with Dom methods */
-	
+
 	update(props){
 		const _ = this;
 		if(!_.initedUpdate){
@@ -86,23 +94,27 @@ export default class G_G{
 			});
 		}
 		if( (!props)  ){
-			_[_.handlersName].forEach( fnObj => {
+			/*_[_.handlersName].forEach( fnObj => {
 				for(let innerProp in fnObj) {
+
 					fnObj[innerProp]();
 				}
-			});
+			});*/
 			return void 0;
 		}
 		_[_.handlersName].forEach( fnObj => {
 			for(let innerProp in fnObj){
 				if(~props.indexOf(innerProp)){
 					fnObj[innerProp]();
-				}else if(innerProp === '')	fnObj[innerProp]();
+				}else if(innerProp === '')	{
+
+					fnObj[innerProp]();
+				}
 			}
 		});
-		
+
 		//
-		
+
 	}
 	_(fn,deps = []){
 		const _ = this;
@@ -120,6 +132,14 @@ export default class G_G{
 				_[_.handlersName].push(propObj);
 			}
 		}
-		
+
+	}
+
+	async ii(props){
+		const _ = this;
+		await _.defineDefineMethod(props);
+		await _.defineInitMethod(props);
+		await _.update();
+		_.initedUpdate = true;
 	}
 }
